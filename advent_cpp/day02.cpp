dbe1b8334b0c2ab4cce15e0a86d171bd599eeeec
Gerbatov <johann.gerberding1@uni-oldenburg.de>
2021-01-31 12:23:46 +0100

    solved first part of day 2 in C++

--- advent_cpp/day02.cpp
+++ advent_cpp/day02.cpp
@@ -2,6 +2,8 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <algorithm>
+#include <cassert>
 
 const std::vector<std::string> INPUT {
     "1-3 a: abcde",
@@ -9,63 +11,67 @@ const std::vector<std::string> INPUT {
     "2-9 c: ccccccccc"
 };
 
-std::vector<std::string> split(std::string pw) 
+std::vector<std::string> read_input(std::string & fileName)
 {
-    std::vector<std::string> pw_parts;
-    // is there a " "
-    int start = 0;
-    size_t pos = pw.find(" ");
-    while (pos != -1) {
-        pw_parts.push_back(pw.substr(start, pos));
-        start = pos;
-        pw = pw.substr(pos+1);
-        pos = pw.find(" ");
+    std::vector<std::string> passwords;
+    std::ifstream in(fileName.c_str());
+
+    // Check if the object is valid
+    if (!in) {
+        std::cerr << "Cannot open the file: " << fileName << std::endl;
     }
-    return pw_parts;
-    
+
+    std::string str;
+    // Read in the file, line by line
+    while (std::getline(in, str)) 
+    {
+        if (str.size() > 0)
+            passwords.push_back(str);
+    }
+    // Close the file
+    in.close();
+
+    return passwords;
 }
 
 int check_passwords(std::vector<std::string> passwords) 
 {
-    int valid_pwds = 0;
-    //for (std::string pwd : passwords) 
-    //{
+    int valid = 0;
+    for (std::string pwd : passwords) 
+    {
+        // remove newline from the end of the pwd
+        pwd.erase(std::remove(pwd.begin(), pwd.end(), '\n'), pwd.end());
+        std::size_t p = pwd.find(" ");
+        // extract min and max number for character
+        std::string min_max = pwd.substr(0, p);
+        std::size_t min = std::stoi(min_max.substr(0, min_max.find("-")));
+        std::size_t max = std::stoi(min_max.substr((min_max.find("-")+1)));
 
-    //}
-    return 0;
+        // extract the character and the password
+        pwd = pwd.substr((p+1));
+        char character = pwd.substr(0, pwd.find(":"))[0];
+        std::string password = pwd.substr((pwd.find(" ") + 1));
+        // count the char occurences
+        std::size_t n = std::count(password.begin(), password.end(), character);
+        if (n >= min and n <= max) {
+            valid++;
+        } 
+
+    }
+    return valid;
 }
 
 int main() 
-{
-    std::string test = "1-3 a: abcde\n";
+{   
+    std::string fileName {"../inputs/day02.txt"};
+    std::vector<std::string> passwords = read_input(fileName);
+    std::cout << "Read input successfully." << std::endl;
 
-    std::vector<std::string> parts = split(test);
-    for (std::string part: parts) {
-        std::cout << part << std::endl;
-    }
+    assert((check_passwords(INPUT) == 2));
+    std::cout << "First test passed!" << std::endl;
 
-    std::size_t pos = test.find(" ");
-    std::string test_2 = test.substr(0, pos);
-    int pos_ = test_2.find("-");
-    std::string min_pos = test_2.substr(0,pos_);
-    std::string max_pos = test_2.substr(pos_+1);
-    int min_c = std::stoi(min_pos);
-    int max_c = std::stoi(max_pos);
-    std::cout << "Min: " << min_c << std::endl;
-    std::cout << "Max: " << max_c << std::endl;
-
-    std::string test_3 = test.substr(pos+1);
-    std::cout << test_3 << std::endl; 
-    std::size_t pos_char = test_3.find(":");
-    std::string c = test_3.substr(0,pos_char);
-    std::cout << "Character: " << c << std::endl;
-
-    pos = test_3.find(" ");
-    std::string test_4 = test_3.substr(pos+1);
-    std::cout << test_4 << std::endl;
-
-    pos_ = test.find("q");
-    std::cout << pos_ << std::endl; 
+    assert((check_passwords(passwords) == 465));
+    std::cout << "Second test passed!" << std::endl;
     
     return 0;
 }
