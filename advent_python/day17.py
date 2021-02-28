4720eaae1bb41dcf5469d813366794cd48e2e881
Gerbatov <johann.gerberding1@uni-oldenburg.de>
2021-02-28 12:58:27 +0100

    solved day17 with help

--- advent_python/day17.py
+++ advent_python/day17.py
@@ -14,33 +14,71 @@ TEST = """.#.
 ..#
 ###"""
 
-grid = collections.defaultdict(int)
 
-for x, row in enumerate(INPUT.split('\n')):
-    for y, val in enumerate(row):
-        if val == '#':
-            grid[(x,y,0)] = 1
+def part_1(raw: str) -> int:
+    grid = collections.defaultdict(int)
 
-for _ in range(6):
-    # create a new neighbors dict
-    neighbors = collections.defaultdict(int)
-    # iterate over active cubes
-    for coord, val in grid.items():
-        if val:
-            # itertools product -> cartesian product
-            for delta in itertools.product([-1, 0, 1], repeat=3):
-                # for any delta thats not (0,0,0)
-                if any(delta):
-                    neighbors[tuple(map(sum, zip(coord, delta)))] += 1
+    for x, row in enumerate(raw.split('\n')):
+        for y, val in enumerate(row):
+            if val == '#':
+                grid[(x,y,0)] = 1
+
+    for _ in range(6):
+        # create a new neighbors dict
+        neighbors = collections.defaultdict(int)
+        # iterate over active cubes
+        for coord, val in grid.items():
+            if val:
+                # itertools product -> cartesian product
+                for delta in itertools.product([-1, 0, 1], repeat=3):
+                    # for any delta thats not (0,0,0)
+                    if any(delta):
+                        neighbors[tuple(map(sum, zip(coord, delta)))] += 1
 
 
 
-    _grid = collections.defaultdict(int)
+        _grid = collections.defaultdict(int)
 
-    for coords, val in neighbors.items():
-        if val == 3 or (grid[coords] and val == 2):
-            _grid[coords] = 1
-    
-    grid = _grid
+        for coords, val in neighbors.items():
+            if val == 3 or (grid[coords] and val == 2):
+                _grid[coords] = 1
+        
+        grid = _grid
 
-print(len(grid))
+    return len(grid)
+
+
+def part_2(raw: str) -> int:
+    grid = collections.defaultdict(int)
+
+    for x, row in enumerate(raw.split('\n')):
+        for y, val in enumerate(row):
+            if val == '#':
+                grid[(x,y,0,0)] = 1
+
+    for _ in range(6):
+        # create a new neighbors dict
+        neighbors = collections.defaultdict(int)
+        # iterate over active cubes
+        for coord, val in grid.items():
+            if val:
+                # itertools product -> cartesian product
+                for delta in itertools.product([-1, 0, 1], repeat=4):
+                    # for any delta thats not (0,0,0,0)
+                    if any(delta):
+                        neighbors[tuple(map(sum, zip(coord, delta)))] += 1
+
+
+
+        _grid = collections.defaultdict(int)
+
+        for coords, val in neighbors.items():
+            if val == 3 or (grid[coords] and val == 2):
+                _grid[coords] = 1
+        
+        grid = _grid
+
+    return len(grid)
+
+print(f"part 1: {part_1(INPUT)}")
+print(f"part 2: {part_2(INPUT)}")
